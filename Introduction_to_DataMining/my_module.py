a017d3dc85cdc8ca50b827639a0df424b79516f0
Kim-Jongchan <71026250+Kim-Jongchan@users.noreply.github.com>
2020-09-12 13:31:08 -0400

    Add files via upload

---
+++ Introduction_to_DataMining/my_module.py
@@ -0,0 +1,11 @@
+message = 'my_module was imported'
+
+print(message)
+
+def coffee_func(person = ''):
+    return 'coffee time for {}'.format(person)
+
+def tea_time(*args):
+    print('tea time for',args)
+    
+    
